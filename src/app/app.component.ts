1623335d72e4d0d28df2517041cbadd4d36a2b22
Prosper Nnadi <prospernnadi3@gmail.com>
2021-01-14 18:03:12 +0100

    Modified app.component.ts and app.component.html to change pagination

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -127,6 +127,9 @@ export class AppComponent {
     {
       title: 'Scenic beaches',
       url: 'https://images.unsplash.com/photo-1506929562872-bb421503ef21?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTh8fGJlYWNofGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60'
-    },
-  ]
+    }
+  ];
+  checkIndex(index: number) {
+    return Math.abs(this.currentPage - index) < 5;
+  }
 }
